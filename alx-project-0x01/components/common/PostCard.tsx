43f805a2747708533bde613755076b0f4d2ec0fe
Gloria <abinezagloria@gmail.com>
2025-06-01 17:22:05 +0300

    completed task 2

--- alx-project-0x01/components/common/PostCard.tsx
+++ alx-project-0x01/components/common/PostCard.tsx
@@ -1,20 +1,15 @@
-import React from 'react';
+import { PostProps } from "@/interfaces";
 
-interface PostCardProps {
-  title: string;
-  body: string;
-  userId: number;
-  id: number;
-}
-
-const PostCard: React.FC<PostCardProps> = ({ title, body, userId, id }) => {
+const PostCard: React.FC<PostProps> = ({ title, body, userId, id }) => {
   return (
-    <div className="bg-white shadow-md rounded-lg p-4 m-2">
-      <h2 className="text-xl font-semibold">{title}</h2>
-      <p>{body}</p>
-      <div className="mt-2 text-sm text-gray-600">
-        <p>User ID: {userId}</p>
-        <p>Post ID: {id}</p>
+    <div className="max-w-xl mx-auto my-6 p-6 bg-white rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
+      <div className="mb-4">
+        <h2 className="text-2xl font-semibold text-gray-800">{title}</h2>
+      </div>
+      <p className="text-gray-600">{body}</p>
+      <div className="mt-4 flex items-center justify-between text-sm text-gray-500">
+        <span>User ID: {userId}</span>
+        <span>Post ID: {id}</span>
       </div>
     </div>
   );
